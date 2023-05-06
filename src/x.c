174106a13b7eb1f81d852522b564bb136cb18793
ng-tony <ng.tony.dev@gmail.com>
2023-05-05 21:08:02 -0400

    Tab bar to bottom

--- src/x.c
+++ src/x.c
@@ -914,6 +914,9 @@ void x_push_node(Con *con) {
             }
         }
         rect.height = max_y + max_height;
+        if (con->layout == L_STACKED || con->layout == L_TABBED)
+            rect.y += con->rect.height - rect.height;
+
         if (rect.height == 0)
             con->mapped = false;
     } else if (con->window == NULL) {
